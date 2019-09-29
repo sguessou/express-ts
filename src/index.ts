184f82cff28b989960c86642a4324d91cbd57e88
Saad Guessous <guessous_saad@hotmail.com>
2019-09-29 15:50:22 +0300

    converted remaining routes logic and handler using decorators and metadata

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import express from "express";
-import { router } from "./routes/loginRoutes";
-import bodyParser from "body-parser";
-import cookieSession from "cookie-session";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({ keys: ["dkjfkld"] }));
-app.use(router);
+app.use(cookieSession({ keys: ['dkjfkld'] }));
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+    console.log('Listening on port 3000');
 });
